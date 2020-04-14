79f48bfe2b39b0ee3e05c41e34944667b9361c3f
AmirH Hassaneini <mytechmix@gmail.com>
2020-04-14 22:22:28 +0200

    only one cout

--- prime/main.cpp
+++ prime/main.cpp
@@ -5,43 +5,42 @@
 
 using namespace std;
 
-int is_prime(string number)
+BN_CTX *bnctx;
+BIGNUM *a;
+BIGNUM *n;
+BIGNUM *e;
+BIGNUM *r;
+
+string is_prime(string number)
 {
     if (number == "2")
-	return 1;
+	return "1";
 
     // Fermat Primality Test
-
-    BN_CTX *bnctx = BN_CTX_new();
-
-    BIGNUM *a = BN_new();
-    BN_dec2bn(&a, string("2").c_str());
-
-    BIGNUM *n = BN_new();
     BN_dec2bn(&n, number.c_str());
-
-    BIGNUM *e = BN_new();
     BN_sub(e, n, BN_value_one());
-
-    BIGNUM *r = BN_new();
-
     BN_mod_exp(r, a, e, n, bnctx);
-
     if (BN_cmp(r, BN_value_one()) == 0)
-        return 1;
+        return "1";
 
-    return 0;
+    return "0";
 }
 
 int main(int argc, char *argv[]) {
 
     ifstream f(argv[1]);
     if (f.is_open()) {
-        string line;
+        string line, output;
+        bnctx = BN_CTX_new();
+        a = BN_new();
+        BN_dec2bn(&a, string("2").c_str());
+        n = BN_new();
+        e = BN_new();
+        r = BN_new();
         while ( getline(f, line) ) {
-            cout << is_prime(line);
-            cout << endl;
+            output += is_prime(line) + "\n";
         }
+        cout << output;
         f.close();
     } else
         cout << "Unable to open file";
