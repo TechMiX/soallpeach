2f52cf904af99885540c51bef41bb7a9bd72a57f
AmirH Hassaneini <mytechmix@gmail.com>
2020-04-14 11:35:50 +0200

    update prime test

--- prime/main.cpp
+++ prime/main.cpp
@@ -1,31 +1,28 @@
 #include <stdio.h>
-#include <cmath>
-
-using namespace std;
+#include <math.h>
 
 typedef unsigned long long uint64;
 
-bool FermatProbablePrimalityTest(uint64 n)
+int is_prime(uint64 n)
 {
-    if (n == uint64(2))
-        return true;
-    uint64 a = 2;
-    uint64 e = n - 1;
-    uint64 r;
-    r = uint64(pow(a, e)) % n;
-    if (r == 1)
-        return true;
+    if (n <= 1) return 0;
+    if (n <= 3) return 1;
+    if (n % 2 == 0) return 0;
 
-    return false;
+    uint64 limit = (uint64)(sqrtl(n));
+    for (uint64 i=2; i<limit; i++) {
+        if (n % i == 0)
+            return 0;
+    }
+    return 1;
 }
 
 int main(int argc, char *argv[]) {
     FILE* fp = fopen(argv[1], "r");
     uint64 i = 0;
-    while (!feof(fp))
-    {
-        fscanf(fp, "%d", &i);
-        printf("%d\n", FermatProbablePrimalityTest(i)? 1 : 0);
+    while (!feof(fp)) {
+        fscanf(fp, "%llu", &i);
+        printf("%d\n", is_prime(i));
     }
     fclose(fp);
     return 0;
