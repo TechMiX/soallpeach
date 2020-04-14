5878cdf99c9a556cf3f771f47f3b6b9edad04e15
AmirH Hassaneini <mytechmix@gmail.com>
2020-04-14 14:57:28 +0200

    add cache

--- prime/main.cpp
+++ prime/main.cpp
@@ -1,19 +1,28 @@
 #include <stdio.h>
 #include <math.h>
+#include <map>
 
 typedef unsigned long long uint64;
+std::map<uint64, int> cache;
 
 int is_prime(uint64 n)
 {
     if (n <= 1) return 0;
-    if (n <= 3) return 1;
-    if (n % 2 == 0) return 0;
+    else if (n <= 3) return 1;
+    else if (n % 2 == 0) return 0;
+
+    std::map<uint64,int>::iterator it = cache.find(n);
+    if (it != cache.end())
+        return it->second;
 
     uint64 limit = (uint64)(sqrtl(n));
     for (uint64 i=2; i<=limit; i++) {
-        if (n % i == 0)
+        if (n % i == 0) {
+            cache.insert(it, std::pair<uint64,int>(n, 0));
             return 0;
+        }
     }
+    cache.insert(it, std::pair<uint64,int>(n, 1));
     return 1;
 }
 
