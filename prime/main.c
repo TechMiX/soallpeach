04de7e6b4847535249e155f77c416a3b816b2d7c
AmirH Hassaneini <mytechmix@gmail.com>
2020-04-17 13:48:13 +0200

    less memory

--- prime/main.c
+++ prime/main.c
@@ -12,17 +12,16 @@ int primes[limit];
 static void sieve() {
     numbers[0] = true; numbers[1] = true;
     for (i=0, primes_count=0; i<limit; i++) {
-	if (numbers[i] == true) continue;
+	if (numbers[i])
+             continue;
         primes[primes_count++] = i;
         for (j=2; i*j<limit; j++) numbers[i*j] = true;
     }
 }
 
 static int is_n_prime() {
-    if (n<limit) {
-        if (!numbers[n]) return 1;
-        return 0;
-    }
+    if (n<limit)
+        return !numbers[n]? 1 : 0;
 
     // if n is bigger than our limit, check primality of n
     for (i=0; i<primes_count; i++)
@@ -40,7 +39,7 @@ int main(int argc, char *argv[]) {
     char *fcontent = malloc(fsize + 1);
     i = fread(fcontent, 1, fsize, fp);
 
-    char* output = (char*)malloc(100000000*sizeof(char)); 
+    char* output = (char*)malloc(3000000*sizeof(char)); 
     char* output_start = output;
     FILE* fpm = fmemopen(fcontent, fsize, "r");
     while (fscanf(fpm, "%d", &n) > 0)
