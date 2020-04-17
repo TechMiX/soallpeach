2a1ae2e9cb8fbd6b33e5b000e703e0380b197540
AmirH Hassaneini <mytechmix@gmail.com>
2020-04-17 15:33:47 +0200

    no functions for less operations

--- prime/main.c
+++ prime/main.c
@@ -1,37 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
 
 #define limit 100000
 
-int i, j, n;
-int primes_count;
-bool numbers[limit];
-int primes[limit];
+int main(int argc, char *argv[]) {
 
-static void sieve() {
-    numbers[0] = true; numbers[1] = true;
+    int i, j, n, primes_count;
+    int numbers[limit];
+    int primes[limit];
+
+    // sieve
+    numbers[0] = 1; numbers[1] = 1;
     for (i=0, primes_count=0; i<limit; i++) {
-	if (numbers[i])
+        if (numbers[i] == 1)
              continue;
         primes[primes_count++] = i;
-        for (j=2; i*j<limit; j++) numbers[i*j] = true;
+        for (j=2; i*j<limit; j++) numbers[i*j] = 1;
     }
-}
 
-static int is_n_prime() {
-    if (n<limit)
-        return !numbers[n]? 1 : 0;
-
-    // if n is bigger than our limit, check primality of n
-    for (i=0; i<primes_count; i++)
-        if (n % primes[i] == 0)
-            return 0;
-    return 1;
-}
-
-int main(int argc, char *argv[]) {
-    sieve();
+    // open and read the whole file at once
     FILE* fp = fopen(argv[1], "r");
     fseek(fp, 0, SEEK_END);
     long fsize = ftell(fp);
@@ -39,11 +26,23 @@ int main(int argc, char *argv[]) {
     char *fcontent = malloc(fsize + 1);
     i = fread(fcontent, 1, fsize, fp);
 
-    char* output = (char*)malloc(3000000*sizeof(char)); 
+    char* output = malloc(3000000*sizeof(char)); 
     char* output_start = output;
     FILE* fpm = fmemopen(fcontent, fsize, "r");
-    while (fscanf(fpm, "%d", &n) > 0)
-        output += sprintf(output, "%d\n", is_n_prime());
+    while (fscanf(fpm, "%d", &n) > 0) {
+        if (n<limit) {
+            output += sprintf(output, "%d\n", numbers[n]^1);
+            continue;
+        }
+
+        // if n is bigger than our limit, check primality of n
+        for (i=0; i<primes_count; i++)
+            if (n % primes[i] == 0) {
+                output += sprintf(output, "%d\n", 0);
+                continue;
+            }
+        output += sprintf(output, "%d\n", 1);
+    }
     fprintf(stdout, "%s", output_start);
     return 0;
 }
