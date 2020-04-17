0bf59581836e5297dce9828332af8f4b781f203a
AmirH Hassaneini <mytechmix@gmail.com>
2020-04-17 02:05:06 +0200

    update primality check

--- prime/main.c
+++ prime/main.c
@@ -20,10 +20,6 @@ static void sieve() {
 }
 
 static int is_n_prime() {
-    if (n <= 1) return 0;
-    else if (n <= 3) return 1;
-    else if (n % 2 == 0 || n % 3 == 0) return 0;
-
     // binary search in primes
     l = 0; r = primes_count - 1;
     while (l <= r) {
@@ -36,18 +32,16 @@ static int is_n_prime() {
     // if n is bigger than our limit, check primality of n
     if (n < limit) return 0;
     j = (int)(sqrt(n));
-    for (i=5; i<=j; i+=6) {
-        if (n % i == 0 || n % (i+2) == 0)
+    for (i=2; i<primes_count; i++)
+        if (n % primes[i] == 0)
             return 0;
-    }
     return 1;
 }
 
 int main(int argc, char *argv[]) {
     sieve();
     FILE* fp = fopen(argv[1], "r");
-    while (fscanf(fp, "%d", &n) == 1) {
+    while (fscanf(fp, "%d", &n) == 1)
         fprintf(stdout, "%d\n", is_n_prime());
-    }
     return 0;
 }
