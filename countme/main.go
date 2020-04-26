569f0d7e80808bafac103a7ce89d34efb75bc132
AmirH Hassaneini <mytechmix@gmail.com>
2020-04-26 21:12:00 +0200

    remove extra jmps

--- countme/main.go
+++ countme/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"io/ioutil"
-	"log"
 	"net"
 	"net/http"
 	"strconv"
@@ -10,36 +9,28 @@ import (
 	"github.com/sasha-s/go-deadlock"
 )
 
-var mutex deadlock.Mutex
 var sum int64
+var mutex deadlock.Mutex
 
 func main() {
 
-	listener, err := net.Listen("tcp", "0.0.0.0:80")
-	if err != nil {
-		log.Println("Error! cannot bind to port 80 :", err)
-		return
-	}
+	listener, _ := net.Listen("tcp", "0.0.0.0:80")
 
 	serverMux := http.NewServeMux()
-	serverMux.HandleFunc("/", SumRoute)
+	serverMux.HandleFunc("/", IncreaseRoute)
 	serverMux.HandleFunc("/count", CountRoute)
 
 	http.Serve(listener, serverMux)
 }
 
-func SumRoute(w http.ResponseWriter, r *http.Request) {
-	body, err := ioutil.ReadAll(r.Body)
-	if err != nil {
-		http.Error(w, "can't read body", http.StatusBadRequest)
-		return
-	}
-
+func IncreaseRoute(w http.ResponseWriter, r *http.Request) {
+	body, _ := ioutil.ReadAll(r.Body)
 	number, _ := strconv.Atoi(string(body[:len(body)-1]))
 
 	mutex.Lock()
 	sum += int64(number)
 	mutex.Unlock()
+
 	w.Write([]byte(strconv.FormatInt(sum, 10)))
 }
 
