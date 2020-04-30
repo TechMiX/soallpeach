c140d45f89c8182ee83b84a68df8b8f519dcba0e
AmirH Hassaneini <mytechmix@gmail.com>
2020-04-30 18:09:37 +0200

    refactor to have less operations + remove deadlock

--- countme/main.go
+++ countme/main.go
@@ -3,40 +3,28 @@ package main
 import (
 	"strconv"
 
-	"github.com/sasha-s/go-deadlock"
 	"github.com/valyala/fasthttp"
 )
 
 var sum int64
-var mutex deadlock.Mutex
 
 func main() {
 
 	m := func(ctx *fasthttp.RequestCtx) {
 		switch string(ctx.Path()) {
 		case "/":
-			IncreaseRoute(ctx)
+			buf := ctx.PostBody()
+			number := int64(0)
+			for i := 0; i < len(buf); i++ {
+				number = (number << 1) + (number << 3) + int64(buf[i]-'0')
+			}
+			sum += number
 		case "/count":
-			CountRoute(ctx)
+			ctx.Write([]byte(strconv.FormatInt(sum, 10)))
 		default:
 			ctx.Error("not found", fasthttp.StatusNotFound)
 		}
 	}
 
 	fasthttp.ListenAndServe(":80", m)
-}
-
-func IncreaseRoute(ctx *fasthttp.RequestCtx) {
-
-	number, _ := strconv.Atoi(string(ctx.PostBody()))
-
-	mutex.Lock()
-	sum += int64(number)
-	mutex.Unlock()
-}
-
-func CountRoute(ctx *fasthttp.RequestCtx) {
-	mutex.Lock()
-	ctx.Write([]byte(strconv.FormatInt(sum, 10)))
-	mutex.Unlock()
 }
